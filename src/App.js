dcd13d069153a9dfff7203e86323b88b6caa0c33
Eric Martin <eric_martin@me.com>
2022-03-10 20:57:44 -0700

    Imported @apollo/client Had to use v3.2.4 as anything newer causes a break in CodeSandbox.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./styles.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://771kbe.sse.codesandbox.io/",
+  cache: new InMemoryCache()
+});
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </ApolloProvider>
   );
 }
